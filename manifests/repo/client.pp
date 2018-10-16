d86ef5f49f129f316e2d010ebc0dcf52386783eb
Lukasz Klimek <lukasz@softinn.eu>
2018-10-16 08:58:35 +0000

    Fix repo URLs in Spacewalk for CentOS

--- manifests/repo/client.pp
+++ manifests/repo/client.pp
@@ -1,19 +1,29 @@
 class spacewalk::repo::client (
   $client_repo_enabled  = '1',
   $client_repo_gpgcheck = '1',
-  $client_repo_release  = 'latest',
-  $client_repo_gpgkey   = 'https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/pubkey.gpg',
+  $client_repo_release  = '2.8',
+  $client_repo_gpgkey   = "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${client_repo_release}-client/pubkey.gpg",
+
+
 ){
 
   case $::osfamily {
     'RedHat': {
+      case $::operatingsystem {
+        'Fedora': {
+          $repo = "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${client_repo_release}-client/fedora-${::operatingsystemmajrelease}-\$basearch"
+        }
+        'RedHat', 'CentOS': {
+          $repo = "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${client_repo_release}-client/epel-${::operatingsystemmajrelease}-\$basearch"
+        }
+      }
 
       yumrepo {'spacewalk-client':
         enabled  => $client_repo_enabled,
         descr    => "Spacewalk Client ${client_repo_release} Repository",
         gpgcheck => $client_repo_gpgcheck,
         gpgkey   => $client_repo_gpgkey,
-        baseurl =>  "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-2.8/fedora-${::operatingsystemmajrelease}-\$basearch/",
+        baseurl =>  $repo,
       }
     }
 
--- manifests/repo/server.pp
+++ manifests/repo/server.pp
@@ -1,7 +1,7 @@
 class spacewalk::repo::server (
   $spacewalk_repo_enabled   = '1',
   $spacewalk_repo_gpgcheck  = '1',
-  $spacewalk_repo_release   = 'latest',
+  $spacewalk_repo_release   = '2.8',
   $spacewalk_repo_gpgkey    = 'https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-2.8/pubkey.gpg',
 
   $copr_repo_enabled    = '1',
@@ -11,13 +11,20 @@ class spacewalk::repo::server (
 
   case $::osfamily {
     'RedHat': {
-
+      case $::operatingsystem {
+        'Fedora': {
+          $repo = "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${spacewalk_repo_release}/fedora-${::operatingsystemmajrelease}-\$basearch/"
+        }
+        'RedHat', 'CentOS': {
+          $repo = "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${spacewalk_repo_release}/epel-${::operatingsystemmajrelease}-\$basearch/"
+        }
+      }
       yumrepo {'spacewalk-project':
         enabled  => $spacewalk_repo_enabled,
         descr    => "Spacewalk ${spacewalk_repo_release} Repository",
         gpgcheck => $spacewalk_repo_gpgcheck,
         gpgkey   => $spacewalk_repo_gpgkey,
-        baseurl  => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-2.8/fedora-${::operatingsystemmajrelease}-\$basearch/",
+        baseurl  => $repo,
     }
 
       yumrepo {'copr-java':
@@ -25,7 +32,7 @@ class spacewalk::repo::server (
         descr         => 'Copr repo for java packages owned by @spacewalkproject',
         gpgcheck      => $copr_repo_gpgcheck,
         gpgkey        => $copr_repo_gpgkey,
-        baseurl       => "https://copr-be.cloud.fedoraproject.org/results/%40spacewalkproject/java-packages/epel-7-x86_64/",
+        baseurl       => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/epel-7-x86_64/",
         repo_gpgcheck => '0',
       }
     }
